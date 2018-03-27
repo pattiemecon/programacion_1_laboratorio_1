3bb74047d6c973f2b0da778b864488be4af1e6d8
pattiemecon <patoattie@gmail.com>
2018-03-27 19:52:14 -0300

    20180327_Clase_07

---
+++ pattie/03_Genericas/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+En casa, validar flotante, char, y doble
+*/
+
+/** \brief Pide un número entero al usuario y lo valida
+ *
+ * \param Mensaje a mostrar al usuario
+ * \param Valor mínimo del intervalo de validación
+ * \param Valor máximo del intervalo de validación
+ * \return El número ingresado por el usuario validado
+ *
+ */
+int pedirEntero(char[], int, int);
+
+/** \brief Pide un número entero al usuario y lo valida
+ *
+ * \param Mensaje a mostrar al usuario
+ * \param Valor mínimo del intervalo de validación
+ * \param Valor máximo del intervalo de validación
+ * \return El número ingresado por el usuario validado
+ *
+ */
+int validarEntero(int, int, int);
+
+int main()
+{
+    int edad;
+    int legajo;
+
+    edad = pedirEntero("Ingrese edad: ", 1, 60);
+
+    legajo = pedirEntero("Ingrese legajo: ", 1, 1500);
+
+    printf("La edad ingresada es %d", edad);
+    printf("\nEl legajo ingresado es %d", legajo);
+
+    return 0;
+}
+
+int pedirEntero(char mensaje[], int minimo, int maximo)
+{
+    int numero;
+
+    printf("%s", mensaje);
+    scanf("%d", &numero);
+
+    numero = validarEntero(numero, minimo, maximo);
+
+    return numero;
+}
+
+int validarEntero(int dato, int minimo, int maximo)
+{
+    while(dato < minimo || dato > maximo)
+    {
+        printf("ERROR Ingrese un numero entre %d y %d: ", minimo, maximo);
+        scanf("%d", &dato);
+    }
+
+    return dato;
+}
